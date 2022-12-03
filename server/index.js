9398b15f6df27aafeed5e4fb3e66ec979887dc79
Asel Williams <aselwilliams22@gmail.com>
2022-12-03 12:47:43 -0600

    hashed the pin

---
+++ server/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+const {createMessage} = require('./controllers/messageController')
+
+app.post('/api/messages', createMessage)
+
+
+app.listen('4004', ()=> console.log(`Server is running on port 4004`))
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express')
+// const cors = require('cors')
+
+// const app = express()
+
+// app.use(express.json())
+// app.use(cors())
+
+// const {
+//     getMessages,
+//     createMessage
+// } = require('./controllers/messageController')
+
+// app.post(`/api/messages`, createMessage)
+
+// app.listen(4004, () => console.log(`running on 4004`))
